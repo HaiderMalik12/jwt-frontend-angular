148275f29b1e35f78a764fd89808485897bfcc20
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-25 02:25:25 +0500

    signup successful

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: "app-auth-form",
@@ -10,7 +11,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
   styleUrl: "./auth-form.component.css",
 })
 export class AuthFormComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   authForm = this.formBuilder.group({
     email: ["", Validators.required],
@@ -20,5 +21,14 @@ export class AuthFormComponent {
   onSubmit() {
     // Send API REQUEST TO BACKEND SERVER
     console.log(this.authForm.value);
+    this.http.post("http://localhost:3001/api/signup", this.authForm.value, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    })
+      .subscribe((data) => {
+        console.log(data);
+        // Do something with the response data
+      });
   }
 }
