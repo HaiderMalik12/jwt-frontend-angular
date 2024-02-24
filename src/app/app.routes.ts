05bc73c5a21ba6f298fdcd39bd440a67fb4689ad
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-25 01:44:05 +0500

    created auth form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AuthFormComponent } from "./auth-form/auth-form.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "signup", component: AuthFormComponent },
+  { path: "", redirectTo: "/signup", pathMatch: "full" },
+];
