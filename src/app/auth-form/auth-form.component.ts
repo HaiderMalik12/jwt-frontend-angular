50ba0678125a0a825a05b3a22946ec392b2b7941
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-25 02:00:35 +0500

    integrated reactive forms for signup

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-auth-form',
+  selector: "app-auth-form",
   standalone: true,
-  imports: [],
-  templateUrl: './auth-form.component.html',
-  styleUrl: './auth-form.component.css'
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: "./auth-form.component.html",
+  styleUrl: "./auth-form.component.css",
 })
 export class AuthFormComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  authForm = this.formBuilder.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required],
+  });
+
+  onSubmit() {
+    // Send API REQUEST TO BACKEND SERVER
+    console.log(this.authForm.value);
+  }
 }
