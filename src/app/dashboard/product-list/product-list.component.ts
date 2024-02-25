ac314717340bb0827ab22e556d0172c3aa3ce6df
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-25 16:36:32 +0500

    render multiple products

--- src/app/dashboard/product-list/product-list.component.ts
+++ src/app/dashboard/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductItemComponent } from "../product-item/product-item.component";
 
 @Component({
-  selector: 'app-product-list',
+  selector: "app-product-list",
   standalone: true,
-  imports: [],
-  templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  imports: [ProductItemComponent],
+  templateUrl: "./product-list.component.html",
+  styleUrl: "./product-list.component.css",
 })
 export class ProductListComponent {
-
 }
